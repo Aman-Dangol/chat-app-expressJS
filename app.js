3e8b56bb6b4106cc8b2e5372dc18f4c5877b9e7e
Aman Dangol <amandangol423@gmail.com>
2024-06-08 13:27:30 +0545

    storing register data in database

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // requiring database
-const conn = require("./db.js");
+const { conn } = require("./db");
 // acquiring cookie-parser
 const cookieParser = require("cookie-parser");
 const app = express();
@@ -9,19 +9,18 @@ const server = http.createServer(app);
 
 // working on socket module
 const { socket } = require("./socket.js");
-const { log } = require("console");
-const exp = require("constants");
 // initializing socket
 socket(server);
 // using cookie parser
-app.use(cookieParser);
+app.use(cookieParser());
+
+// setting view engine to ejs
 app.set("view engine", "ejs");
 
 app.use("/public", express.static("./public"));
+
 // getting body parameters
 app.use(express.urlencoded({ extended: false }));
-
-// cookie parser
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -35,11 +34,16 @@ app.get("/register", (req, res) => {
 //sign-up process
 
 app.post("/signup", (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.send("sign up page");
+  const { username, email, password } = req.body;
+  console.log(username, email, password);
+  conn.query(
+    `insert into users(username,email,password) values('${username}','${email}','${password}')`
+  );
+  res.redirect("/login");
 });
 
+//provide login page
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 server.listen(8000);
-
-console.log(conn);
